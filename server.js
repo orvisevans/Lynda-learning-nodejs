9d5de21f065bce2a9a85b193d3c764dd1655027b
orvis <b.orvis@gmail.com>
2018-01-24 22:02:10 -0800

    init commit (through video 4:2)

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var messages = [
+    {name: 'Joyce', message: 'Hello'},
+    {name: 'Bobbi', message: 'Howdy'}
+]
+
+app.get('/messages', (req, res) =>{
+    res.send(messages);
+});
+
+app.post('/messages', (req, res) =>{
+    messages.push(req.body);
+    res.sendStatus(200);
+});
+
+var server = app.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+});
