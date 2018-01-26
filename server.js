546d0797df4c9262f795d2219a1da5869474e503
orvis <b.orvis@gmail.com>
2018-01-25 20:00:03 -0800

    Finished tutorial, through socketio and jasmine

--- server.js
+++ server.js
@@ -1,25 +1,55 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var messages = [
-    {name: 'Joyce', message: 'Hello'},
-    {name: 'Bobbi', message: 'Howdy'}
-]
+var dbUrl = 'mongodb://user:user@ds115758.mlab.com:15758/lynda-learn-nodejs'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
 
 app.get('/messages', (req, res) =>{
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
 });
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body);
-    res.sendStatus(200);
+app.get('/messages/:user', (req, res) =>{
+    var user = req.params.user;
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages);
+    })
 });
 
-var server = app.listen(3000, () => {
+app.post('/messages', async (req, res) =>{
+    try {
+        var message = new Message(req.body);
+        var savedMessage = await message.save();
+        console.log('message saved: ', message.message);
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+        return console.error(error);
+    }
+});
+
+io.on('connection',(socket) => {
+    console.log("user connected");
+});
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongodb connection', err);
+});
+
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 });
